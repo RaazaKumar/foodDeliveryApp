ff0f3d3096028f357e15e8a34e7921a25f679420
RaazaKumar <xtrajakumar@gmail.com>
2025-05-03 19:31:09 +0530

    `Update Navbar component: change 'bhoom' to 'Home' in NavLink`

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       </Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
         <NavLink to="/" className="flex flex-col items-center gap-1">
-          <p>bhoom</p>
+          <p>Home</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden " />
         </NavLink>
         <NavLink to="/collection" className="flex flex-col items-center gap-1">
